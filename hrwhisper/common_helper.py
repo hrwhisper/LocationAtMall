f52cae186766cdd1a4641657ab5973e279f5aa3d
hrwhisper <hrwhisper@qq.com>
2017-10-22 15:53:17 +0800

    1. add visual class XXToVec to easy add new vector. 2. refactor many class.

--- hrwhisper/common_helper.py
+++ hrwhisper/common_helper.py
@@ -8,9 +8,7 @@ from scipy import sparse
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-
-from hrwhisper.parse_data import read_mall_data, read_train_data
+from parse_data import read_mall_data, read_train_data, read_test_data
 
 
 def trained_and_predict_location(cls, X_train, y_train, X_test):
@@ -21,18 +19,96 @@ def trained_and_predict_location(cls, X_train, y_train, X_test):
     return predicted
 
 
-class ModelBase(ABC):
-    def __init__(self, test_size=0.2, random_state=42):
-        self._test_size = test_size
+def train_test_split(X, y, test_size=0.2):
+    train_size = int((1 - test_size) * X.shape[0])
+    return X.iloc[:train_size], X.iloc[train_size:], y.iloc[:train_size], y.iloc[train_size:]
+
+
+class XXToVec(ABC):
+    def __init__(self, feature_save_path, how_to_vec_save_path):
+        self.FEATURE_SAVE_PATH = feature_save_path
+        self.HOW_TO_VEC_SAVE_PATH = how_to_vec_save_path
+
+    @abc.abstractclassmethod
+    def train_data_to_vec(self, train_data, mall_id, renew=True, should_save=False):
+        """
+
+        :param train_data:
+        :param mall_id:
+        :param renew:
+        :param should_save:
+        :return:
+        """
+
+    @abc.abstractclassmethod
+    def test_data_to_vec(self, test_data, mall_id, renew=True, should_save=False):
+        """
+
+        :param test_data:
+        :param mall_id:
+        :param renew:
+        :return:
+        """
+
+
+class ModelBase(object):
+    def __init__(self, test_ratio=0.2, random_state=42):
+        self._test_ratio = test_ratio
         self._random_state = random_state
 
-    def get_classifiers(self):
+    def get_name(self):
+        return self.__class__.__name__
+
+    def _get_classifiers(self):
         """
         :return: dict. {name:classifer}
         """
         return {
-            'random forest': RandomForestClassifier(n_jobs=-1, n_estimators=100, random_state=42),
+            'random forest': RandomForestClassifier(n_jobs=3, n_estimators=100, random_state=self._random_state),
         }
+
+    def _trained_by_mall_and_predict_location(self, vec_func, train_data, test_data, has_test_label=True):
+        """
+
+        :param vec_func:
+        :param train_data:
+        :param test_data:
+        :param has_test_label: bool
+        :return:
+        """
+        ans = {}
+        for mall_id in train_data['mall_id'].unique():
+            vectors = []
+            for func in vec_func:
+                vectors.append(func.train_data_to_vec(train_data, mall_id))
+            X_train = sparse.hstack(vectors)
+            y_train = train_data.loc[train_data['mall_id'] == mall_id]['shop_id']
+
+            vectors = []
+            for func in vec_func:
+                vectors.append(func.test_data_to_vec(test_data, mall_id))
+
+            X_test = sparse.hstack(vectors)
+            assert X_test.shape[0] == len(test_data.loc[test_data['mall_id'] == mall_id])
+
+            print(X_train.shape, y_train.shape)
+            print(X_test.shape)
+            if has_test_label:
+                y_test = test_data.loc[test_data['mall_id'] == mall_id]['shop_id']
+                print(y_test.shape)
+
+            classifiers = self._get_classifiers()
+            for name, cls in classifiers.items():
+                predicted = trained_and_predict_location(cls, X_train, y_train, X_test)
+                if has_test_label:
+                    score = accuracy_score(y_test, predicted)
+                    ans[name] = ans.get(name, 0) + score
+                    print(mall_id, name, score)
+                else:
+                    for row_id, label in zip(test_data.loc[test_data['mall_id'] == mall_id]['row_id'], predicted):
+                        ans[row_id] = label
+                        # joblib.dump(cls, './model_save/use_wifi_{}_{}.pkl'.format(name, mall_id))
+        return ans
 
     def train_test(self, vec_func):
         """
@@ -40,33 +116,27 @@ class ModelBase(ABC):
         :param vec_func: list of vector function
         :return:
         """
+        # ------input data -----------
         mall_data = read_mall_data()
         train_data = read_train_data()  # 1138015
         train_data = train_data.join(mall_data.set_index('shop_id'), on='shop_id', rsuffix='_mall')
+        train_data = train_data.sort_values(by='time_stamp')
+        train_label = train_data['shop_id']
+        train_data, test_data, _, _ = train_test_split(train_data, train_label, self._test_ratio)
 
-        total = collections.Counter()
-        cnt = 0
-        for mall_id in train_data['mall_id'].unique():
-            vectors = []
-            for func in vec_func:
-                vectors.append(func(train_data, mall_id))
-            # X_train = wifi_to_vec(train_data, mall_id)
-            X_train = sparse.hstack(vectors)
-            y_train = train_data.loc[train_data['mall_id'] == mall_id]['shop_id']
-            X_train, X_test, y_train, y_test = train_test_split(X_train, y_train,
-                                                                test_size=self._test_size,
-                                                                random_state=self._random_state)
-            # print(X_train.shape,y_train.shape)
-            # print(X_test.shape,y_test.shape)
-            cnt += 1
-            classifiers = self.get_classifiers()
-            for name, cls in classifiers.items():
-                # cls = KNeighborsClassifier()
-                # print(name)
-                predicted = trained_and_predict_location(cls, X_train, y_train, X_test)
-                score = accuracy_score(y_test, predicted)
-                total[name] += score
-                print(mall_id, name, score)
-                joblib.dump(cls, './model_save/use_wifi_{}_{}.pkl'.format(name, mall_data))
-        for name, score in total.items():
+        cnt = train_data['mall_id'].unique().shape[0]
+        total_cnt = self._trained_by_mall_and_predict_location(vec_func, train_data, test_data, has_test_label=True)
+        for name, score in total_cnt.items():
             print("{} Mean: {}".format(name, score / cnt))
+
+    def train_and_on_test_data(self, vec_func):
+        mall_data = read_mall_data()
+        train_data = read_train_data()  # 1138015
+        train_data = train_data.join(mall_data.set_index('shop_id'), on='shop_id', rsuffix='_mall')
+        test_data = read_test_data()
+        ans = self._trained_by_mall_and_predict_location(vec_func, train_data, test_data, False)
+
+        with open('./res.csv', 'w') as f:
+            f.write('row_id,shop_id\n')
+            for row_id in test_data['row_id']:
+                f.write('{},{}\n'.format(row_id, ans[row_id]))
--- hrwhisper/use_wifi.py
+++ hrwhisper/use_wifi.py
@@ -4,80 +4,115 @@
 """
     使用了wifi特征，类似BOW
 """
-import pandas as pd
-import numpy as np
-import collections
-from scipy.sparse import csr_matrix, hstack
-from sklearn import naive_bayes, svm, linear_model
+from scipy.sparse import csr_matrix
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.externals import joblib
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn import preprocessing
-
-from hrwhisper.parse_data import read_train_data, read_mall_data
-from hrwhisper.common_helper import trained_and_predict_location, ModelBase
-
-
-def location_to_vec(train_data, mall_id):
-    """
-
-    :param train_data:
-    :return: csr_matrix
-    """
-    if train_data is None:
-        train_data = read_train_data()  # 1138015
-    location_feature = train_data[['longitude', 'latitude']]
-    # return csr_matrix(location_feature.values)
-    return preprocessing.scale(location_feature.values)
+from parse_data import read_train_data
+from common_helper import ModelBase, XXToVec
 
 
-def wifi_to_vec(train_data, mall_id):
-    """
-    :param data: pandas. train_data.join(mall_data.set_index('shop_id'), on='shop_id', rsuffix='_mall')
-    :param mall_id: str
-    :return: csr_matrix
-    """
-    # _wifi_feature_save_path = './feature_save/wifi_features.pkl'
-    # _wifi_bssid_save = './feature_save/wifi_bssid.pkl'
-    wifi_bssid = set()
-    min_strong = 0x7fffffff
+# def location_to_vec(train_data, mall_id):
+#     """
+#
+#     :param train_data:
+#     :return: csr_matrix
+#     """
+#     if train_data is None:
+#         train_data = read_train_data()  # 1138015
+#     location_feature = train_data[['longitude', 'latitude']]
+#     # return csr_matrix(location_feature.values)
+#     return preprocessing.scale(location_feature.values)
 
-    train_data = train_data.loc[train_data['mall_id'] == mall_id]
-    for wifi_infos in train_data['wifi_infos']:
-        for wifi in wifi_infos.split(';'):
-            _id, _strong, _connect = wifi.split('|')
-            wifi_bssid.add(_id)
-            min_strong = min(min_strong, int(_strong) - 100)
 
-    wifi_bssid = {_id: i for i, _id in enumerate(sorted(wifi_bssid))}
-    indptr = [0]
-    indices = []
-    data = []
-    for wifi_infos in train_data['wifi_infos']:
-        for wifi in wifi_infos.split(';'):
-            _id, _strong, _connect = wifi.split('|')
-            _id = wifi_bssid[_id]
-            indices.append(_id)
-            data.append(int(_strong) - min_strong)
-        indptr.append(len(indices))
+class WifiToVec(XXToVec):
+    def __init__(self):
+        super().__init__('./feature_save/wifi_features_{}_{}.pkl', './feature_save/wifi_bssid_{}_{}.pkl')
+        self.min_strong = -300
 
-    wifi_features = csr_matrix((data, indices, indptr), dtype=int)  # TODO normalize
-    # return preprocessing.scale(wifi_features, with_mean=False)
-    # joblib.dump(wifi_features, _wifi_feature_save_path)
-    # joblib.dump(wifi_bssid, _wifi_bssid_save)
-    return wifi_features
+    def train_data_to_vec(self, train_data, mall_id, renew=True, should_save=False):
+        """
+        :param data: pandas. train_data.join(mall_data.set_index('shop_id'), on='shop_id', rsuffix='_mall')
+        :param mall_id: str
+        :param renew: renew the feature
+        :param should_save: bool, should save the feature on disk or not.
+        :return: csr_matrix
+        """
+        if renew:
+            wifi_bssid = set()
+
+            train_data = train_data.loc[train_data['mall_id'] == mall_id]
+            for wifi_infos in train_data['wifi_infos']:
+                for wifi in wifi_infos.split(';'):
+                    _id, _strong, _connect = wifi.split('|')
+                    wifi_bssid.add(_id)
+
+            wifi_bssid = {_id: i for i, _id in enumerate(sorted(wifi_bssid))}
+            indptr = [0]
+            indices = []
+            data = []
+            for wifi_infos in train_data['wifi_infos']:
+                for wifi in wifi_infos.split(';'):
+                    _id, _strong, _connect = wifi.split('|')
+                    _id = wifi_bssid[_id]
+                    indices.append(_id)
+                    data.append(int(_strong) - self.min_strong)
+                indptr.append(len(indices))
+
+            wifi_features = csr_matrix((data, indices, indptr), dtype=int)  # TODO normalize
+            # return preprocessing.scale(wifi_features, with_mean=False)
+            if should_save:
+                joblib.dump(wifi_features, self.FEATURE_SAVE_PATH.format('train', mall_id))
+            joblib.dump(wifi_bssid, self.HOW_TO_VEC_SAVE_PATH.format('train', mall_id))
+        else:
+            wifi_features = joblib.load(self.FEATURE_SAVE_PATH.format('train', mall_id))
+        return wifi_features
+
+    def test_data_to_vec(self, test_data, mall_id, renew=True, should_save=False):
+        if renew:
+            wifi_bssid = joblib.load(self.HOW_TO_VEC_SAVE_PATH.format('train', mall_id))
+            indptr = [0]
+            indices = []
+            data = []
+            test_data = test_data.loc[test_data['mall_id'] == mall_id]
+            not_in = set()
+            for wifi_infos in test_data['wifi_infos']:
+                for wifi in wifi_infos.split(';'):
+                    _id, _strong, _connect = wifi.split('|')
+                    if _id in wifi_bssid:
+                        _id = wifi_bssid[_id]
+                        indices.append(_id)
+                        data.append(int(_strong) - self.min_strong)
+                    else:
+                        not_in.add(_id)
+                indptr.append(len(indices))
+            # for i in range(len(wifi_bssid) - len(indptr)):
+
+            # indptr.extend([len(indices)] * (len(wifi_bssid) - len(indptr)))
+            print('total: {} ,not_in :{}'.format(len(wifi_bssid), len(not_in)))
+            wifi_features = csr_matrix((data, indices, indptr), dtype=int, shape=(len(test_data), len(wifi_bssid)))
+            # TODO normalize
+            # return preprocessing.scale(wifi_features, with_mean=False)
+            if should_save:
+                joblib.dump(wifi_features, self.FEATURE_SAVE_PATH.format('test', mall_id))
+        else:
+            wifi_features = joblib.load(self.FEATURE_SAVE_PATH.format('test', mall_id))
+        return wifi_features
 
 
 class UseWifi(ModelBase):
     def __init__(self):
         super().__init__()
 
+    def _get_classifiers(self):
+        return {
+            'random forest': RandomForestClassifier(n_jobs=-1, n_estimators=100, random_state=self._random_state),
+        }
 
 def train_test():
     task = UseWifi()
-    task.train_test([wifi_to_vec])
+    task.train_test([WifiToVec()])
+    # task.train_and_on_test_data([WifiToVec()])
 
 
 if __name__ == '__main__':
