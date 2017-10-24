109ddcc4aa79dfbdebf453c12a1959736b6a492b
hrwhisper <hrwhisper@qq.com>
2017-10-24 15:01:29 +0800

    1. update visualization.

--- hrwhisper/mall_location_visualization.py
+++ hrwhisper/mall_location_visualization.py
@@ -16,14 +16,19 @@ def show_plt():
     plt.show()
 
 
-def only_mall_visualization():
+def only_mall_visualization(mall_id=None):
     train_data = read_mall_data()  # read_train_join_mall()
-    print(train_data.size)
+    if mall_id:
+        train_data = train_data[train_data['mall_id'] == mall_id]
     x = train_data['latitude']
     y = train_data['longitude']
 
-    id2color = {mall_id: i for i, mall_id in enumerate(train_data['mall_id'].unique())}
-    colors = [id2color[i] for i in train_data['mall_id']]
+    if mall_id:
+        id2color = {shop_id: i for i, shop_id in enumerate(train_data['shop_id'].unique())}
+        colors = [id2color[i] for i in train_data['shop_id']]
+    else:
+        id2color = {mall_id: i for i, mall_id in enumerate(train_data['mall_id'].unique())}
+        colors = [id2color[i] for i in train_data['mall_id']]
     plt.scatter(x, y, s=100, c=colors, alpha=0.5)
     show_plt()
 
@@ -56,10 +61,9 @@ def shop_mall_visualization(mall_id='m_4572'):
     show_plt()
 
 
-
 if __name__ == '__main__':
-    # only_mall_visualization()
-    shop_mall_visualization()
+    only_mall_visualization('m_1621')
+    # shop_mall_visualization('m_1621')
     # (Lat_A, Lng_A) = (32.060255, 118.796877)
     # (Lat_B, Lng_B) = (39.904211, 116.407395)
     # print(gpxpy.geo.haversine_distance(Lat_A, Lng_A, Lat_B, Lng_B))
