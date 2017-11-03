36ebadb8f8f5e418fe69dc5964d79abc19aa5df9
hrwhisper <hrwhisper@qq.com>
2017-11-03 20:53:20 +0800

    fix grid search bugs.

--- hrwhisper/grid_search.py
+++ hrwhisper/grid_search.py
@@ -15,11 +15,9 @@ from use_wifi import WifiToVec
 from use_wifi_kstrong import WifiKStrongToVec
 
 
-def grid_search_xgboost():
-    n_cpus = get_recommend_cpu_count()
-
+def multiclass_xgboost():
     parameters = {'nthread': [2],
-                  'objective': ['binary:logistic'],
+                  'objective': ['multi:softmax'],
                   'learning_rate': [0.025, 0.05, 0.1],
                   'max_depth': [6, 7, 8, 10],
                   'min_child_weight': [1, 5, 10],
@@ -29,11 +27,32 @@ def grid_search_xgboost():
                   'n_estimators': [100, 200, 500],
                   'missing': [-999],
                   'random_state': [1337, 1080, 1024, 1226]}
+    clf = GridSearchCV(XGBClassifier(), parameters, n_jobs=get_recommend_cpu_count() // 2,
+                       cv=KFold(n_splits=5, random_state=42),
+                       verbose=1, refit=True)
+    return clf
 
-    xgb_model = XGBClassifier()
-    clf = OneVsRestClassifier(GridSearchCV(xgb_model, parameters, n_jobs=n_cpus // 2,
-                                           cv=KFold(n_splits=5, random_state=42),
-                                           verbose=1, refit=True))
+
+def binary_xgboost():
+    parameters = {'estimator__nthread': [2],
+                  'estimator__objective': ['binary:logistic'],
+                  'estimator__learning_rate': [0.025, 0.05, 0.1],
+                  'estimator__max_depth': [6, 7, 8, 10],
+                  'estimator__min_child_weight': [1, 5, 10],
+                  'estimator__silent': [1],
+                  'estimator__subsample': [0.6, 0.8, 0.9, 1],
+                  'estimator__colsample_bytree': [0.7, 0.8, ],
+                  'estimator__n_estimators': [100, 200, 500],
+                  'estimator__missing': [-999],
+                  'estimator__random_state': [1337, 1080, 1024, 1226]}
+
+    clf = GridSearchCV(OneVsRestClassifier(XGBClassifier()), parameters, n_jobs=get_recommend_cpu_count() // 2,
+                       cv=KFold(n_splits=5, random_state=42),
+                       verbose=1, refit=True)
+    return clf
+
+
+def grid_search_xgboost(clf):
     train_data = read_train_join_mall()
     train_data = train_data.sort_values(by='time_stamp')
     train_label = preprocessing.LabelEncoder().fit_transform(train_data['shop_id'])
@@ -46,10 +65,11 @@ def grid_search_xgboost():
     clf.fit(X_train, y_train)
     print('fit done')
     best_parameters, score, _ = max(clf.grid_scores_, key=lambda x: x[1])
-    print('Raw AUC score:', score)
-    for param_name in sorted(best_parameters.keys()):
-        print("%s: %r" % (param_name, best_parameters[param_name]))
+
+    print('score:', clf.best_score_)
+    for name, val in clf.best_params_.items():
+        print("{}  {}".format(name, val))
 
 
 if __name__ == '__main__':
-    grid_search_xgboost()
+    grid_search_xgboost(binary_xgboost())
